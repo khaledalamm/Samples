5bec98cd40cae78a122a82129e540a2dbf815cdb
khaled <khaled.alamm2@gmail.com>
2017-06-26 04:24:46 +0300

    Add files via upload

---
+++ Module2/Module2.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Console
+Module module1
+
+    Sub main()
+
+        Dim i As Double, l As Double
+        Write("l=")
+        l = ReadLine()
+1:      i = i + 1
+        Dim n = i, k As Double
+        k = (n * (n + 1) / 2)
+        Do Until n > l
+            WriteLine(k & "   " & n)
+
+            GoTo 1
+        Loop
+
+        ReadLine()
+    End Sub
+End Module
