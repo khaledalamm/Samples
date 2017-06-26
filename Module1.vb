ab01263ed492b27bfec8e55a23e4447fb92fbfb4
khaled <khaled.alamm2@gmail.com>
2017-06-26 04:15:10 +0300

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Console
+Module Module1
+    Sub Main()
+        Dim i As Integer, l As Integer
+        Write("l=")
+        l = readline()
+1:      i = i + 1
+        Do Until i > l
+            WriteLine("i=" & i)
+            GoTo 1
+        Loop
+        ReadLine()
+
+    End Sub
+
+End Module
